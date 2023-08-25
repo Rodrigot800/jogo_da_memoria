882c831f96d03446cab100364bdf1c6aa262e3f8
Rodrigo Peixoto <RODRIGOPEIXOTOT800@GMAIL.COM>
2023-08-25 18:13:25 -0300

    1.3

---
+++ script/game.js
@@ -0,0 +1,95 @@
+const grid = document.querySelector('.grid')
+
+const frontCards = [
+    'jujursuKaisen',
+    'berserk',
+    'bokuNoHero',
+    'chawman',
+    'goku',
+    'ichigo',
+    'kimetsunoyaba',
+    'naruto',
+    'saitama',
+    'snk',
+]
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag)
+    element.className = className
+    return element;
+}
+
+let firstCard = ''
+let secondCard = ''
+
+const checkCards = () => {
+    const firstFrontCard = firstCard.getAttribute('data-frontCard')
+    const secondFrontCard = secondCard.getAttribute('data-frontCard')
+
+    if (firstFrontCard === secondFrontCard) {
+        firstCard = ''
+        secondCard = ''
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove('revealcard')
+            secondCard.classList.remove('revealcard')
+
+            firstCard = ''
+            secondCard = ''
+        }, 500)
+
+    
+    }
+
+
+}
+
+const revealcard = ({ target }) => {
+
+    if (target.parentNode.className.includes('revealcard')) {
+        return
+    }
+
+    if (firstCard === '') {
+        target.parentNode.classList.add('revealCard')
+        firstCard = target.parentNode
+    } else if (secondCard === ''){
+        target.parentNode.classList.add('revealCard')
+        secondCard = target.parentNode
+
+        checkCards()
+    }
+
+    target.parentNode.classList.add('revealcard')
+}
+
+const creatCard = (frontCard) => {
+
+    const card = createElement('div', 'card')
+    const front = createElement('div', 'face front')
+    const back = createElement('div', 'face back')
+
+    front.style.backgroundImage = `url('../imagens/${frontCard}.png')`
+
+    card.appendChild(front)
+    card.appendChild(back)
+
+    card.addEventListener('click', revealcard)
+    card.setAttribute('data-frontCard', frontCard)
+    
+    return card
+}
+
+const loadGame = () => {
+
+    const duplicateFrontcards = [...frontCards, ...frontCards]
+    
+    const shuffledArray = duplicateFrontcards.sort( () => Math.random() -0.5 )
+
+    shuffledArray.forEach((frontCard) => {
+        const card = creatCard(frontCard)
+        grid.appendChild(card)
+    })
+}
+
+loadGame()
